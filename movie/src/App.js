e47570334e693b23aa282e4ad940970aea67504a
Haile Melaku <haile4cmd@gmail.com>
2022-01-02 12:32:15 +0300

    creating this react app and tailwind css

---
+++ movie/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <header className="App-header">
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <h1 className="text-3xl font-bold underline bg-amber-400">
+        {" "}
+        Hello world!{" "}
+      </h1>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
+}
+export default App;
