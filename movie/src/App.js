adec2b5c2ded8ac70adaa238febd8b2d6285728f
Haile Melaku <haile4cmd@gmail.com>
2022-01-02 13:03:33 +0300

    adding all the components and pages

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,24 +1,13 @@
-import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <header className="App-header">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <h1 className="text-3xl font-bold underline bg-amber-400">
-        {" "}
-        Hello world!{" "}
-      </h1>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <main className="w-screen h-screen bg-neutral-700"></main>
+      </>
+    );
+  }
 }
+
 export default App;
